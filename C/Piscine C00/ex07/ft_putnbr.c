fb3a682d12f177d5efefd20e0ced2210294c77fe
Mr.Salih <salihbaz07@gmail.com>
2021-12-05 17:14:12 +0300

    edit

--- C/Piscine C00/ex07/ft_putnbr.c
+++ C/Piscine C00/ex07/ft_putnbr.c
@@ -1,21 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   t_main.c                                           :+:      :+:    :+:   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: spirnaz <42istanbul.com.tr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/30 02:34:30 by spirnaz           #+#    #+#             */
-/*   Updated: 2021/11/30 03:27:34 by spirnaz          ###   ########.fr       */
+/*   Created: 2021/11/30 02:34:12 by spirnaz           #+#    #+#             */
+/*   Updated: 2021/11/30 02:34:19 by spirnaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putnbr(int nb);
+#include <unistd.h>
 
-int	main(void)
+void	ft_putchar(char c)
 {
-	int	nb;
+	write(1, &c, 1);
+}
 
-	nb = 64;
-	ft_putnbr(nb);
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar('8');
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else
+	{
+		if (nb > 9)
+		{
+			ft_putnbr(nb / 10);
+		}
+		ft_putchar(48 + nb % 10);
+	}
 }
