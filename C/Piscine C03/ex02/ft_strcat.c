9d9e273c3ef9ea90fea9831be8d1b86f72c6f366
Mr.Salih <salihbaz07@gmail.com>
2021-12-07 15:29:59 +0300

    C03 add

---
+++ C/Piscine C03/ex02/ft_strcat.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spirnaz <42istanbul.com.tr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/07 12:45:07 by spirnaz           #+#    #+#             */
+/*   Updated: 2021/12/07 14:23:45 by spirnaz          ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src [j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/*
+int	main()
+{
+	char src[] = "Benguerir"
+                  "asdasd"
+                 "asdasdsada";
+	char dest[] = "1337 ";
+	printf("%s", ft_strcat(dest, src));
+}
+*/
---
+++ C/Piscine C03/ex05/ft_strlcat.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spirnaz <42istanbul.com.tr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/07 15:26:22 by spirnaz           #+#    #+#             */
+/*   Updated: 2021/12/07 15:28:31 by spirnaz          ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	dlen;
+	unsigned int	slen;
+
+	i = 0;
+	j = 0;
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+	dlen = j;
+	slen = ft_strlen(src);
+	if (size == 0 || size <= dlen)
+		return (slen + size);
+	while (src [i] != '\0' && i < size - dlen - 1)
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
+	return (dlen + slen);
+}
+/*
+int main (void)
+{
+	char src[] = "Born to code";
+    	char dest [] = "1337 42";
+    printf("%i \n", ft_strlcat(dest, src, 20));
+    printf("%s \n", dest);
+}
+*/
