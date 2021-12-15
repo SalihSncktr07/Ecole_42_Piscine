80aafbf839f92c2db4ddd0a5f00911e44833d260
Mr.Salih <salihbaz07@gmail.com>
2021-12-15 12:36:48 +0300

    c08 fix

---
+++ C/C08/ex04/ft_stock_str.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spirnaz <42istanbul.com.tr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/15 12:36:07 by spirnaz           #+#    #+#             */
+/*   Updated: 2021/12/15 12:36:10 by spirnaz          ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STOCK_STR_H
+# define FT_STOCK_STR_H
+
+typedef struct s_stock_str
+{
+	int			size;
+	char		*str;
+	char		*copy;
+}				t_stock_str;
+
+#endif
